8ad19e30ddf2cbdbfe25e908f1e7e12875a8f173
Gadiel Kalleb <gadiel_kalleb@hotmail.com>
2018-10-23 23:19:03 -0300

    criação da autenticação e login

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Rooms from './Rooms'
 class App extends Component {
   constructor(props) {
     super(props)
+    
     this.state = {
       
     }
--- src/Login.js
+++ src/Login.js
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom'
+import axios from 'axios';
 
-const Login = (props) => {
-  return (
-    <div className="container-2 w-container">
-      <form className="lobby" method="post">
-        <h1 className="heading">Seja bem-vindo</h1>
-        <div>Informe seu nome para começar:</div>
-        <input className="div-block-3" name="name" style={{ "width": "100%" }}/><br/>
-        <input type="submit" className="w-button" value="Entrar" />
-      </form>
-    </div>
-  );
+class Login extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      success: false
+    }
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  handleSubmit (e) {
+    axios.post('http://localhost:3030/auth', {
+      name: this.name.value
+    }).then( out => {
+      const token = out.data.token
+      window.localStorage.setItem('token', token)
+      this.setState({
+        success: true
+      })
+    })
+    e.preventDefault()
+  }
+  render () {
+    if (this.state.success) {
+      return <Redirect to='/rooms' />
+    }
+    return (
+      <div className="container-2 w-container">
+        <form className="lobby" method="post" onSubmit={this.handleSubmit}>
+          <h1 className="heading">Seja bem-vindo</h1>
+          <div>Informe seu nome para começar:</div>
+          <input className="div-block-3" name="name" style={{ "width": "100%" }} ref={(ref) => this.name = ref}/><br/>
+          <input type="submit" className="w-button" value="Entrar" />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Login
